0e84122887c782cf027d044c002fffaf43a1541f
Duda Tomasio <dudasoarestomasio@gmail.com>
2023-05-20 20:06:48 -0300

    adicionando comentários

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -7,9 +7,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
-		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1")); //valor do bônus
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			valor = BigDecimal.ZERO;  //se o bônus do funcionário passar de mil reias, o valor assumirá 0
 		}
 		return valor;
 	}
