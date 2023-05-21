fb54467369ba7bb9be82eee9993185cfd30ff8a9
Duda Tomasio <dudasoarestomasio@gmail.com>
2023-05-20 23:14:13 -0300

    lançando exception

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -8,9 +8,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
-		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1")); //valor do bônus
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1")); //valor do bônus (10%)
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;  //se o bônus do funcionário passar de mil reias, o valor assumirá 0
+			throw new IllegalArgumentException("Funcionários com salário acima de R$10.000 não podem receber bônus."); //se o bônus do funcionário passar de mil reias, o valor assumirá 0
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
